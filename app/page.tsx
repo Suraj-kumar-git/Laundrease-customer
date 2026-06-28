73c5c472db1239ee0fdedc1c89756919c05f3f30
sudhir kumar patel <sudhir.kumar2020@vitbhopal.ac.in>
2026-06-28 22:01:52 +0530

    Added Layout.tsx for deployment on onrender

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,5 @@
 import { redirect } from 'next/navigation';
 
 export default function RootRedirect() {
-  const role = process.env.ROLE || null;
-  redirect(`/${role}`);
+  redirect('/customer');
 }
