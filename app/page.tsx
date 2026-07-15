b031bdf66947b7853e5211109d83cd92c30b4240
sudhir kumar patel <sudhir.kumar2020@vitbhopal.ac.in>
2026-07-16 02:16:54 +0530

    Third commit to trigger the build

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
 export default function RootRedirect() {
-  const role = process.env.ROLE || null;
-  redirect(`/${role}`);
+  // const role = process.env.ROLE || null;
+  redirect('/customer');
 }
