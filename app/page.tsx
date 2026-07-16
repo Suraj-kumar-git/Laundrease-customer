960e8b8503a4301882b8c5afd4d8d726b23a246c
sudhir kumar patel <sudhir.kumar2020@vitbhopal.ac.in>
2026-07-17 00:42:52 +0530

    Revert "Third commit to trigger the build"
    This reverts commit b031bdf66947b7853e5211109d83cd92c30b4240.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
 export default function RootRedirect() {
-  // const role = process.env.ROLE || null;
-  redirect('/customer');
+  const role = process.env.ROLE || null;
+  redirect(`/${role}`);
 }
