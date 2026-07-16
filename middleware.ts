08898305f6579348ba0ac6b346344fa577b937a9
sudhir kumar patel <sudhir.kumar2020@vitbhopal.ac.in>
2026-07-17 01:22:08 +0530

    Trying with automatic migration to opennext

--- middleware.ts
+++ middleware.ts
@@ -134,7 +134,7 @@ async function verifyRefreshTokenSignature(token: string): Promise<Awaited<Retur
   }
 }
  
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
   // Skip middleware for static files
   if (
